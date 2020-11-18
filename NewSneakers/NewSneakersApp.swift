89eccd901118900201a416886f0fdc9ac04dffbe
Junho Yeo <hanaro0704@gmail.com>
2020-11-18 23:35:28 +0900

    Initial Commit

---
+++ NewSneakers/NewSneakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSneakersApp.swift
+//  NewSneakers
+//
+//  Created by Junho Yeo on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct NewSneakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
