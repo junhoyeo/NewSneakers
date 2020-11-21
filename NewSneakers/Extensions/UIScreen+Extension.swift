a2992433fb99684cfa819caac1b73d55a1ae1580
Junho Yeo <hanaro0704@gmail.com>
2020-11-21 12:45:05 +0900

    refactor: detach Extensions

---
+++ NewSneakers/Extensions/UIScreen+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen+Extension.swift
+//  NewSneakers
+//
+//  Created by Junho Yeo on 2020/11/21.
+//
+
+import SwiftUI
+
+extension UIScreen{
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+}
